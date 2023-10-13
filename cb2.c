b6e1da639378fe47f7e1b3f3993ae984c3cde22a
srushtigadale <147224463+srushtigadale@users.noreply.github.com>
2023-10-13 07:17:45 +0530

    Create cb2.c

---
+++ cb2.c
@@ -0,0 +1,16 @@
+// C program to demonstrate hexadecimal to decimal 
+// conversion using format specifier 
+
+#include <stdio.h> 
+
+int main() 
+{ 
+
+	int n; 
+	// taking input from user 
+	scanf("%x", &n); 
+	
+	// printing the decimal number 
+	printf("%d", n); 
+	return 0; 
+}
